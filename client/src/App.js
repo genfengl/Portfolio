5d4bd5317296eae5fcb2c1e75f195d3123fd3cb2
genfengl <genfeng.liu@gmail.com>
2022-11-14 20:26:04 +0800

    skills section in progress

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,19 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import About from "./components/About"
 import Play from "./components/Play";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <div className="relative w-full max-w">
       
-      <div className="bg-materialBlack text-word flex flex-col gap-6">
+      <div className="bg-materialBlack text-word flex flex-col gap-6 overflow-hidden">
         
         <Navbar />
         <Hero />
         <Play />
         <About />
+        <Skills />
       </div>
 
     </div>
